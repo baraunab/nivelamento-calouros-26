6fb61f7a378fd91df07c682a163c8edcc29ad3f0
baraunab <bbarauna.beatriz@gmai.com>
2026-07-20 16:20:58 -0400

    primeiro commit

---
+++ exercicio1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// isso é um comentário !!
+
+/*
+ isso também é um comentário,
+ mas com várias linhas !!
+*/
+
+// função principal - tudo que está aqui dentro será interpretado
+int main(){
+    int idade = 21; // número inteiro
+    float altura = 1.75; // número decimal
+    char inicial = 'C'; // caractere
+
+    printf("Idade: %d\n", idade);
+    printf("Altura: %f\n", altura);
+    printf("Inicial de Carlos: %c\n", inicial);
+
+    return 0; // retorno do tipo int
+}
